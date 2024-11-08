103ee33cdade3ed645cb0bf2faf88e4831c1673d
gulcan <gulcanncee@gmail.com>
2024-11-08 19:41:35 +0300

    Update AppMain.java

--- src/main/java/com/gulcancelik/AppMain.java
+++ src/main/java/com/gulcancelik/AppMain.java
@@ -3,6 +3,6 @@ package com.gulcancelik;
 public class AppMain {
 
     public static void main(String[] args) {
-        System.out.println("Selam");
+        System.out.println("Selam"); 
     }
 }
